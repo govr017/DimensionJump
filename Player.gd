7f6c378bd740ccb7f0e485213582c0fa5c05de4a
Slava Razbitskiy <v.razbitskiy@alterosmart.com>
2020-08-27 15:29:07 +0500

    cleanup: rename .gd files as .tscn

--- Player.gd
+++ Player.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-
-
 var aud_player = AudioStreamPlayer2D.new()
 var motion = Vector2()
 const GRAVITY = 5
@@ -9,30 +7,17 @@ const SPEED = 100
 const UP = Vector2(0, -1 )
 const JUMP_HEIGHT = -150
 
-
-
-
-
-
-
-
 func _physics_process(delta):
 	motion.y += GRAVITY
-	
-	
-	
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
 		$Sprite.flip_h = false
 		$Sprite.play("Walk")
-		
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
 		$Sprite.flip_h = true
 		$Sprite.play("Walk")
-	
-	
 	else:
 		motion.x = 0
 		$Sprite.play("idle")
@@ -43,25 +28,8 @@ func _physics_process(delta):
 			self.add_child(aud_player)
 			aud_player.stream = load("res://sfx/Jump.wav")
 			aud_player.play()
-			
-			
-			
 	else:
 		$Sprite.play("Jump")
 	
-	
-		
-	
-	
-	
-		
-		
-		
-		
-	
-	
 	motion = move_and_slide(motion, UP)
 	pass
-
-
-
